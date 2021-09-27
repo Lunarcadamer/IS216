e6b56ebe964b033d4ad49fdb61d5ce53f053cd9c
Aloysius Chia <wasp.7000@gmail.com>
2021-09-27 14:49:30 +0800

    Did Week 6 Exercises

--- session6/challenges/Bootstrap5/resources/js/challenge13.js
+++ session6/challenges/Bootstrap5/resources/js/challenge13.js
@@ -7,16 +7,64 @@
 function display_default() {
 
     // YOUR CODE GOES HERE
-    // Call API
-
+    let gender = 'm'; // male
+    if( Math.random() < 0.5 ) {
+        gender = 'f'; // female
+    }
+    
+    callAPI(gender)
+    process_buttons(gender)
+    
 }
 
+function callAPI(gender) {
+    // Call API
+    let url = "http://krazywoman.com/krazystars/api/star/search.php?g=" + gender
+    axios.get(url)
+    .then(response => {
+        //console.log(response)
+        displayData(response)
+    })
+    .catch(error => {
+        console.log(error.message)
+    });
+}
+
+function displayData(response) {
+    //console.log(response.data.records)
+    celebs = response.data.records
+
+    let wiki_links_str = ''
+    let imdb_links_str = ''
+    let slideshow = ''
+    let active = 'active'
+    for (celeb of celebs) {
+        //console.log(celeb['photo_background_url'])
+        slideshow += `
+        <div class="carousel-item ${active}">
+            <img id="image${celeb['id']}" src="${celeb['photo_background_url']}" alt="">
+            <div class="carousel-caption">
+                <h2 class="star_h2" id="slide_heading3" style="padding: 5px; background-color: grey; color: white">${celeb['fullname']}</h2>
+                <p id="slide_title3" style="padding: 5px; background-color: black; color: white">${celeb['quote']}</p>
+            </div>
+        </div>`
+
+        active = ''
+        wiki_links_str += `<a id="wiki${celeb['id']}" class="dropdown-item" href="${celeb['wikipedia_url']}" target="_blank">${celeb['fullname']}</a>`
+        imdb_links_str += `<a id="imdb${celeb['id']}" class="dropdown-item" href="${celeb['imdb_url']}" target="_blank">${celeb['fullname']}</a>`
+    }
+
+    document.getElementById("wiki_links").innerHTML = wiki_links_str;
+    document.getElementById("imdb_links").innerHTML = imdb_links_str;
+    document.getElementById("slide_show").innerHTML = slideshow;
+}
 
 // This function is called when user clicks on "Show Male Stars" button.
 function show_male_stars() {
 
     // YOUR CODE GOES HERE
-
+    callAPI('m')
+    process_buttons('m')
 }
 
 
@@ -24,5 +72,23 @@ function show_male_stars() {
 function show_female_stars() {
 
     // YOUR CODE GOES HERE
+    callAPI('f')
+    process_buttons('f')
+}
 
+// Active/Disable buttons
+function process_buttons(gender) {
+    // Disable the other gender button
+    // Enable the current gender button
+    if( gender == 'm' ) {
+        // current gender is male
+        document.getElementById("male_button").disabled = true;
+        document.getElementById("female_button").disabled = false;
+    }
+    else {
+        // current gender is female
+        document.getElementById("male_button").disabled = false;
+        document.getElementById("female_button").disabled = true;
+        //console.log("Hello");
+    }
 }
